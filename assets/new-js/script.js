4495a7e525340bc8cec5828292ccd3507c833ec1
HermineHoromyan <hermine.horomyan@gmail.com>
2020-06-16 22:29:50 +0400

    changes

--- assets/new-js/script.js
+++ assets/new-js/script.js
@@ -211,3 +211,37 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+$(function () {
+    var Accordion = function (el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+  
+      // Variables privadas
+      var links = this.el.find('.h-dropdown');
+      // Evento
+      links.on('click', {
+        el: this.el,
+        multiple: this.multiple
+      }, this.dropdown)
+    }
+  
+    Accordion.prototype.dropdown = function (e) {
+      var $el = e.data.el;
+      $this = $(this),
+          $next = $this.next();
+  
+      $next.slideToggle();
+      $this.parent().toggleClass('h-open');
+  
+      if (!e.data.multiple) {
+        $el.find('.h-adrenaline-dropdown').not($next).slideUp().parent().removeClass('h-open');
+      };
+    }
+  
+    var accordion = new Accordion($('#my-accordion'), false);
+  });
+  
+  
